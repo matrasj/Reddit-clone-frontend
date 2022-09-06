b231ae6469e8e1ea3d4bf9137f1cd96566841c3b
pozerinhooo <jkob.matras@gmail.com>
2022-09-06 10:29:54 +0200

    Added responsive features and profile CRUD functionalities.

---
+++ src/app/user-profile/edit-social-media-link/edit-social-media-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-social-media-link',
+  templateUrl: './edit-social-media-link.component.html',
+  styleUrls: ['./edit-social-media-link.component.css']
+})
+export class EditSocialMediaLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
